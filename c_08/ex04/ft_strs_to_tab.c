211215cb7c65e56e24e6a9a2a78485cfe21794a8
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-04-28 12:21:27 +0200

    ex05

--- c_08/ex04/ft_strs_to_tab.c
+++ c_08/ex04/ft_strs_to_tab.c
@@ -6,12 +6,14 @@
 /*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/28 07:34:29 by johnavar          #+#    #+#             */
-/*   Updated: 2023/04/28 11:07:49 by johnavar         ###   ########.fr       */
+/*   Updated: 2023/04/28 11:57:03 by johnavar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_stock_str.h"
 #include <stdlib.h>
+
+typedef struct s_stock_str	t_stock_str;
 
 int	ft_strlen(char *str)
 {
@@ -31,9 +33,8 @@ char	*ft_strdup(char *src)
 
 	l = 0;
 	n = 0;
-	while (src[l])
-		l++;
-	ptr = malloc(sizeof(src) * l + 1);
+	l = ft_strlen(src);
+	ptr = malloc(sizeof(char) * (l + 1));
 	if (ptr == NULL)
 		return (NULL);
 	while (src[n])
@@ -50,17 +51,21 @@ struct	s_stock_str	*ft_strs_to_tab(int ac, char **av)
 	t_stock_str	*tab;
 	int			i;
 
+	if (ac <= 0 || av == NULL)
+		return (NULL);
 	i = 0;
 	tab = malloc(sizeof(t_stock_str) * (ac + 1));
 	if (tab == NULL)
 		return (NULL);
 	while (i < ac)
 	{
-		if (tab[i].str == NULL || tab[i].copy == NULL)
+		tab[i].str = av[i];
+		if (tab[i].str == NULL)
 			return (NULL);
 		tab[i].size = ft_strlen(av[i]);
 		tab[i].copy = ft_strdup(av[i]);
-		tab[i].str = av[i];
+		if (tab[i].copy == NULL)
+			return (NULL);
 		i++;
 	}
 	tab[i].str = NULL;
