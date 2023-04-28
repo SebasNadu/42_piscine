dfa6e36b0ace2f1e777dff9aaa49c07ea823abdb
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-04-28 11:10:34 +0200

    ex04 done

---
+++ c_08/ex04/ft_stock_str.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_stock_str.h                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/04/28 07:27:21 by johnavar          #+#    #+#             */
+/*   Updated: 2023/04/28 07:29:33 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_STOCK_STR_H
+# define FT_STOCK_STR_H
+
+typedef struct s_stock_str
+{
+	int		size;
+	char	*str;
+	char	*copy;
+}	t_stock_str;
+
+#endif
