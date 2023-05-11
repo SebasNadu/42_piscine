fa55a9926239feaf4f464868697aa05ed5a1d198
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-11 22:05:33 +0200

    ex06 done

---
+++ c_11/ex06/ft_sort_string_tab.c
@@ -0,0 +1,87 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_string_tab.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/11 21:04:34 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/11 22:04:37 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+
+void	ft_strswap(char **str1, char **str2)
+{
+	char	*temp;
+
+	temp = *str1;
+	*str1 = *str2;
+	*str2 = temp;
+}
+
+int	ft_strcmp(char *s1, char *s2)
+{
+	int	i;
+
+	i = 0;
+	while (s1[i] && s1[i] == s2[i])
+		i++;
+	return (s1[i] - s2[i]);
+}
+
+void	ft_sort_string_tab(char **tab)
+{
+	int	i;
+	int	j;
+	int	len;
+
+	i = 0;
+	len = 0;
+	while (tab[len])
+		len++;
+	while (0 < len)
+	{
+		i = 0;
+		while (i < len - 1)
+		{
+			if (ft_strcmp(tab[i], tab[i + 1]) > 0)
+				ft_strswap(&tab[i], &tab[i + 1]);
+			i++;
+		}
+		len--;
+	}
+}
+/*
+int	main(void)
+{
+	int		index;
+	char	**tab;
+
+	tab = (char **)malloc(8 * sizeof(char *));
+	tab[0] = strdup("hello");
+	tab[1] = strdup("world");
+	tab[2] = strdup("a");
+	tab[3] = strdup("aa");
+	tab[4] = strdup("ba");
+	tab[5] = strdup("ab");
+	tab[6] = strdup("z");
+	tab[7] = 0;
+	index = 0;
+	while (tab[index])
+	{
+		printf("tab[%d] <@ %p> = $%s$\n", index, tab + index, tab[index]);
+		index++;
+	}
+	ft_sort_string_tab(tab);
+	printf("\n");
+	index = 0;
+	while (tab[index])
+	{
+		printf("tab[%d] <@ %p> = $%s$\n", index, tab + index, tab[index]);
+		index++;
+	}
+} */
