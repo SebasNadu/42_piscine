c56bdab73dd31efb477c51d94d0b76a9dcf8e4d9
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-12 15:54:06 +0200

    ex03

---
+++ c_12/ex03/ft_list_last.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_last.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/12 15:49:58 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/12 15:51:10 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+t_list	*ft_list_last(t_list *begin_list)
+{
+	while (begin_list->next != 0)
+	{
+		begin_list = begin_list->next;
+	}
+	return (begin_list);
+}
