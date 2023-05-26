3d5c0e789fcf98abc9b58b70bc5754f0528530fd
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-26 16:26:54 +0200

    ex08 done

---
+++ c_12/ex08/ft_list_reverse.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_reverse.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 15:59:42 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/26 16:26:31 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_reverse(t_list **begin_list)
+{
+	t_list	*prev;
+	t_list	*next;
+
+	if (*begin_list == 0)
+		return ;
+	while (*begin_list)
+	{
+		next = (*begin_list)->next;
+		(*begin_list)->next = prev;
+		prev = *begin_list;
+		*begin_list = next;
+	}
+	*begin_list = prev;
+}
