a99a3a3e0ab8651730885f750cd7a0c9eadf5d0e
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-05-26 16:37:11 +0200

    ex09 done

---
+++ c_12/ex09/ft_list.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list.h                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 12:38:52 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/26 15:46:57 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_LIST_H
+# define FT_LIST_H
+
+# include <stdlib.h>
+
+typedef struct s_list
+{
+	struct s_list	*next;
+	void			*data;
+}				t_list;
+
+#endif
---
+++ c_12/ex09/ft_list_foreach.c
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_foreach.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: johnavar <johnavar@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/26 16:29:26 by johnavar          #+#    #+#             */
+/*   Updated: 2023/05/26 16:35:25 by johnavar         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_foreach(t_list *begin_list, void (*f)(void *))
+{
+	if (!begin_list || !(*f))
+		return ;
+	(*f)(begin_list->data);
+	ft_list_foreach(begin_list->next, f);
+}
