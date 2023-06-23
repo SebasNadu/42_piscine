9ff0ad20c6f0a673766ab5e5f2469821c7c33961
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-06-23 19:59:26 +0200

    c_13 01

---
+++ c_13/c_01/btree_apply_prefix.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_prefix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 19:01:02 by sebasnadu         #+#    #+#             */
+/*   Updated: 2023/06/23 19:45:35 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_prefix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	applyf(root->item);
+	btree_apply_prefix(root->left, applyf);
+	btree_apply_prefix(root->right, applyf);
+}
