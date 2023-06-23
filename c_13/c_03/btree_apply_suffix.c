1dc3e71b21493c38260d38865d5bbb017992cd28
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-06-23 20:32:04 +0200

    c_13 03

---
+++ c_13/c_03/btree_apply_suffix.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_apply_suffix.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 20:28:51 by sebasnadu         #+#    #+#             */
+/*   Updated: 2023/06/23 20:31:01 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_apply_suffix(t_btree *root, void (*applyf)(void *))
+{
+	if (!root)
+		return ;
+	if (root->left)
+		btree_apply_suffix(root->left, applyf);
+	if (root->right)
+		btree_apply_suffix(root->right, applyf);
+	applyf(root->item);
+}
