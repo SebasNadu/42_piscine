db5e13566141fcb3922786cded8248a61c5e4d2d
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-06-23 20:53:50 +0200

    c_13 04

---
+++ c_13/c_04/btree_insert_data.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_insert_data.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/06/23 20:42:13 by sebasnadu         #+#    #+#             */
+/*   Updated: 2023/06/23 20:53:28 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+void	btree_insert_data(t_btree **root, void *item,
+						int (*cmpf)(void *, void *))
+{
+	if (!*root)
+	{
+		*root = btree_create_node(item);
+		return ;
+	}
+	if (cmpf((*root)->item, item) > 0)
+		btree_insert_data(&(*root)->left, item, cmpf);
+	else
+		btree_insert_data(&(*root)->right, item, cmpf);
+}
