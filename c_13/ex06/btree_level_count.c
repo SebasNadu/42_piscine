2b79a11676f294c306aded8db08833d6b62c96f3
Sebastian Navarro <sebastiannavarro.fl@gmail.com>
2023-07-01 15:45:00 +0200

    ex06

---
+++ c_13/ex06/btree_level_count.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   btree_level_count.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sebasnadu <johnavar@student.42berlin.de>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/01 15:38:59 by sebasnadu         #+#    #+#             */
+/*   Updated: 2023/07/01 15:44:35 by sebasnadu        ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_btree.h"
+
+int	max(int a, int b)
+{
+	if (a > b)
+		return (a);
+	return (b);
+}
+
+int	btree_level_count(t_btree *root)
+{
+	if (!root)
+		return (0);
+	return (1 + max(btree_level_count(root->left),
+			btree_level_count(root->right)));
+}
